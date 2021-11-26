12c6a0d682d66295cd14627119bd5968f4dff975
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-11-26 15:19:01 +0100

    Add GlobalUsings

---
+++ SETraining.Server/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Identity.Web;
+global using Microsoft.Identity.Web.Resource;
+global using Microsoft.OpenApi.Models;
+global using System.Text.Json.Serialization;
+
+global using SETraining.Core;
+global using SETraining.Infrastructure;
