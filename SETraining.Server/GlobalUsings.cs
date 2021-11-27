d1f6fb818a91cf333b40e40ebaf4bf263c7f856e
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-11-27 11:31:12 +0100

    Fix global usings

--- SETraining.Server/GlobalUsings.cs
+++ SETraining.Server/GlobalUsings.cs
@@ -1,11 +1,10 @@
 // global using Microsoft.EntityFrameworkCore;
-// global using Microsoft.AspNetCore.Authorization;
-// global using Microsoft.AspNetCore.Authentication.JwtBearer;
-// global using Microsoft.AspNetCore.Mvc;
-// global using Microsoft.Identity.Web;
-// global using Microsoft.Identity.Web.Resource;
-// global using Microsoft.OpenApi.Models;
-// global using System.Text.Json.Serialization;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Identity.Web;
+global using Microsoft.Identity.Web.Resource;
+global using Microsoft.OpenApi.Models;
+global using System.Text.Json.Serialization;
 
-// global using SETraining.Core;
-// global using SETraining.Infrastructure;
+global using SETraining.Core;
