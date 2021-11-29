9c71551f708727398bd5fa7f3fec74b9acce9307
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-11-29 15:31:24 +0100

    add user class

---
+++ Infrastructure/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE_Traing.Infrastructure;
+public abstract class User
+{
+    
+   [Required]
+   [StringLength(75)]
+   public string Name { get; set; } 
+
+
+   [EmailAddressAttribute]
+   public string Email { get; set; }
+}
