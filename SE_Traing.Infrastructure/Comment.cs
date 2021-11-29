e843d54664b04e9216eb0bad04f17e0b7b4ec64b
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-11-29 14:58:57 +0100

    add classes for model
    wiuth User and tag class

---
+++ SE_Traing.Infrastructure/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE_Traing.Infrastructure;
+public class Comment
+{
+    
+    public int UserId { get; set; } 
+
+    [StringLength(500)]
+    [Required]
+    public string UserComment { get; set; }
+}
---
+++ SE_Traing.Infrastructure/Rating.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE_Traing.Infrastructure;
+public class Rating
+{   
+    public int UserId { get; set; } 
+    public int UserRating { get; set; }
+}
