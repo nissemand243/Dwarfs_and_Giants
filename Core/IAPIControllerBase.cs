f6692c69e14304b4a9bf5fbd1c4215a69851e2d5
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-11-29 16:37:20 +0100

    add controller interfaces

---
+++ Core/IAPIControllerBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Core;
+
+public interface IAPIControllerBase
+{
+    Task<Status> PatchComment(int id, CommentDTO comment);
+    Task<Status> PatchRating(int id, RatingDTO rating);
+    Task<(Status, MaterialDTO)> Get(int id);
+    Task<(Status, IReadOnlyCollection<MaterialDTO>)> Search(string searchInput);
+}
---
+++ Core/IAPIControllerModerator.cs
@@ -0,0 +1,8 @@
+﻿namespace Core;
+
+public interface IAPIControllerModerator
+{
+    Task<Status> DeleteMaterial(int materialId);
+    Task<Status> PutMaterial(int materialId, MaterialUpdateDTO material);
+    Task<(Status, MaterialDTO)> PostMaterial(int materialId, MaterialCreateDTO material); 
+}
