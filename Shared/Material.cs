b4d622fcd64fdcbdde3b9242ef8e099f2ce7a22c
coha <coha@itu.dk>
2021-11-29 13:53:54 +0100

    Added a simple table to show material

---
+++ Shared/Material.cs
@@ -0,0 +1,20 @@
+namespace SE_training.Client.Shared;
+
+public class Material
+{
+
+    public int Id { get; set; }
+    public string Title { get; set; }
+    
+    public string Decription {get;set; }
+    
+    //list of tags
+    public List<string> Tags = new List<string>()
+                    {
+                        "Docker",
+                        "C#",
+                        "Blazor",
+                        "BDSA"                    
+                    };
+
+}
