9b186125a8c579634460013ef5998f16df462090
AntonFriis <anlf@itu.dk>
2021-12-06 00:14:05 +0100

    Added docker setup and start script, but needs fixing.

---
+++ Start.ps1
@@ -0,0 +1,10 @@
+﻿    mkdir /.local
+    Write-Output '*' > .local/.gitignore
+    
+    $password = [guid]::NewGuid().ToString()
+    
+    Write-Output $password > .local/db_password.txt
+    Write-Output "Server=localhost;Database=SE_training;User Id=sa;Password=$password;Trusted_Connection=False;Encrypt=False" > .local/connection_string.txt
+
+    Write-Host "Starting App"
+    docker compose up
