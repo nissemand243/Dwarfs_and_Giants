6acc283d3a838b12c625e98921d60f3cc1ba8337
nissemand243 <xander.s234@gmail.com>
2021-12-06 09:02:59 +0100

    trying to resolve merge conflics

--- Infrastructure/Material.cs
+++ Infrastructure/Material.cs
@@ -4,18 +4,13 @@ public class Material
 {
 
     public int Id { get; set; }
-
-    public int UserId { get; set; }
-
-    public string? Title { get; set; }
-    public string? Description { get; set; }
+    public int AuthorId { get; set; }
+    public string Name { get; set; }  
+    public string Description { get; set; } 
     public FileType FileType { get; set; }
-
     public ICollection<string>? Tags { get; set; }
-
     public ICollection<Comment>? Comments { get; set; }
-
-    public IDictionary<int, int>? Ratings { get; set; }
+    public IDictionary<int, int>? Ratings { get; set; } //UserId to ratingValue
 
     [Url]
     public string? FilePath { get; set; }
