6939e8e192563c28b566ea4dd4bdae27c1ff1894
MaCoHa <74664350+MaCoHa@users.noreply.github.com>
2021-12-06 15:50:18 +0100

    merge main into (#98)
    * remove storage folder (#90)
    
    * Solve merge (#96)
    
    * reinserted test files
    
    * renamed UserId -> Id
    
    * resolved test conflict
    
    * resolved test fail
    
    Co-authored-by: Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
    Co-authored-by: jeroleski <74965501+jeroleski@users.noreply.github.com>

---
+++ Infrastructure.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+global using System;
+global using Xunit;
+
+global using SE_training.Infrastructure;
+global using SE_training.Core;
+global using static SE_training.Core.Status;
--- Infrastructure/Student.cs
+++ Infrastructure/Student.cs
@@ -2,5 +2,6 @@ namespace SE_training.Infrastructure;
 
 public class Student : User
 {
-    
+        public override string TypeOfToString() => "Student";
+
 }
