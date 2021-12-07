279601e4bd69fb1317c9d81ae1714192fabd427c
ThomasITU <75098556+ThomasITU@users.noreply.github.com>
2021-12-07 20:29:11 +0100

    Add RESTful handles for swagger (#110)
    * add classes for model
    
    wiuth User and tag class
    
    * remove unused folder
    
    * Add http handles

--- Server/Controllers/APIControllerModerator.cs
+++ Server/Controllers/APIControllerModerator.cs
@@ -1,3 +1,5 @@
+using SE_training.Infrastructure;
+
 namespace SE_training.Server.Controllers;
 
 [Authorize]
@@ -16,16 +18,24 @@ public class APIControllerModerator : APIControllerBase, IAPIControllerModerator
         _logger = logger;
     }
 
+    //[Authorize(User = $"Teacher")]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public Task<Status> DeleteMaterial(int MaterialId)
     {
         throw new NotImplementedException();
     }
 
+    //[Authorize(User = $"Teacher")]
+    [HttpPost]
     public Task<(Status, MaterialDTO)> PostMaterial(int MaterialId, CreateMaterialDTO material)
     {
         throw new NotImplementedException();
     }
 
+    //[Authorize(User = $"Teacher")] 
+    [HttpPost("{MaterialId}")]
     public Task<Status> PutMaterial(int MaterialId, MaterialDTO material)
     {
         throw new NotImplementedException();
