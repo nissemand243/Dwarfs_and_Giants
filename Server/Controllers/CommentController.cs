8350d5dd1088d3408660d43425169efd5468ba5f
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-07 21:21:23 +0100

    Add the CRUD methods in controllers
    By using the existing repository methods

--- Server/Controllers/CommentController.cs
+++ Server/Controllers/CommentController.cs
@@ -1,3 +1,4 @@
+
 namespace SE_training.Server.Controllers
 {
     public class CommentController : ControllerBase
@@ -10,5 +11,28 @@ namespace SE_training.Server.Controllers
             _logger = logger;
             _repository = repository;
         }
+
+        public Task<Status> DeleteComment(int commentId)
+        {
+            var status = _repository.DeleteAsync(commentId);
+
+            return status;
+        }
+
+        public Task<IReadOnlyCollection<CommentDTO>> ReadAll(int materialId)
+        {
+            return _repository.GetAsync(materialId);
+        }
+
+        public Task<(Status status, CommentDTO Comment)> CreateComment(CreateCommentDTO comment)
+        {
+            var response = _repository.PutAsync(comment);
+            return response;
+        }
+
+        public Task<(Status status, CommentDTO comment)> UpdateComment(CreateCommentDTO comment)
+        {
+            return _repository.PutAsync(comment);
+        }    
     }
 }
--- Server/Controllers/RatingController.cs
+++ Server/Controllers/RatingController.cs
@@ -10,5 +10,26 @@ namespace SE_training.Server.Controllers
             _logger = logger;
             _repository = repository;
         }
+
+        public Task<Status> DeleteRating(int ratingID)
+        {
+            return _repository.DeleteAsync(ratingID);
+        }
+
+        public Task<Status> CreateRating(RatingDTO rating)
+        {
+            return _repository.PostAsync(rating);
+        } 
+
+        public Task<IReadOnlyCollection<RatingDTO>> ReadAllRatings(int MaterialId)
+        {
+            return _repository.GetAsync(MaterialId);
+        }
+
+        public Task<(Status status, RatingDTO Rating)> UpdateRating(CreateRatingDTO rating)
+        {
+            return _repository.PutAsync(rating);
+        }
+
     }
 }
