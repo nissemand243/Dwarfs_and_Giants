34d0e83964a231b46c1c1eef4cd6eee46bdd84d8
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-08 15:47:41 +0100

    adjust interface
    The material id would not be accessible before posting it

--- Core/IAPIControllerModerator.cs
+++ Core/IAPIControllerModerator.cs
@@ -1,8 +1,10 @@
 ﻿namespace SE_training.Core;
 
+
 public interface IAPIControllerModerator
 {
     Task<Status> DeleteMaterial(int MaterialId);
     Task<Status> PutMaterial(int MaterialId, MaterialDTO material);
-    Task<(Status, MaterialDTO)> PostMaterial(int MaterialId, CreateMaterialDTO material);
+    Task<(Status, MaterialDTO)> PostMaterial(CreateMaterialDTO material);
 }
+
