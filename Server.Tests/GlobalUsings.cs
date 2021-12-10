1d5c80a8403c63933e136bac9c6a25c9956f05ea
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-10 14:34:19 +0100

    add moq package

--- Server.Tests/GlobalUsings.cs
+++ Server.Tests/GlobalUsings.cs
@@ -2,3 +2,5 @@ global using Xunit;
 global using SE_training.Server;
 global using SE_training.Core;
 global using SE_training.Infrastructure;
+global using Moq;
+global using System.Threading.Tasks;
