bc6946bd2a80c5f2cd3ba0f9cc8ce729d00c5677
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-10 15:36:32 +0100

    add packages

--- Server.Tests/GlobalUsings.cs
+++ Server.Tests/GlobalUsings.cs
@@ -1,6 +1,9 @@
 global using Xunit;
-global using SE_training.Server;
-global using SE_training.Core;
-global using SE_training.Infrastructure;
 global using Moq;
 global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
+
+global using SE_training.Server.Controllers;
+global using SE_training.Core;
+global using SE_training.Infrastructure;
