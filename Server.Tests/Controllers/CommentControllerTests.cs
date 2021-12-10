f3488eb85581bbf62dff002284dd67beda16e315
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-10 13:27:17 +0100

    Initial setup

---
+++ Server.Tests/Controllers/CommentControllerTests.cs
@@ -0,0 +1,12 @@
+namespace SE_training.Server.Tests.Controllers;
+
+[Fact]
+public void CreateComment_creates_comment_returns_comment_and_status()
+{
+    // Given
+    var mockRepository = new Mock<ICommentRepository>();
+
+    // When
+
+    // Then
+}
---
+++ Server.Tests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using Xunit;
+global using SE_training.Server;
+global using SE_training.Core;
+global using SE_training.Infrastructure;
