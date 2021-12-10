cc3e865f961330aaac4ad82e33310ac90d60e698
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-10 17:16:05 +0100

    Add CreateComment test

--- Server.Tests/Controllers/CommentControllerTests.cs
+++ Server.Tests/Controllers/CommentControllerTests.cs
@@ -1,12 +1,29 @@
+
+
 namespace SE_training.Server.Tests.Controllers;
 
-[Fact]
-public void CreateComment_creates_comment_returns_comment_and_status()
+public class CommentControllerTests
 {
-    // Given
-    var mockRepository = new Mock<ICommentRepository>();
+    [Fact]
+    public async Task CreateComment_creates_comment_returns_comment_and_status()
+    {
+        // Arrange
+        var createCommentDto = new CreateCommentDTO(1,1,"Lorem Ipsum");
+        var commentDTO = new CommentDTO(1,1,1,"Lorem Ipsum");
+        var Expected = (Status.Created, commentDTO);
 
-    // When
+        var mockRepository = new Mock<ICommentRepository>();
+        mockRepository.Setup(repo => repo.CreateAsync(createCommentDto)).ReturnsAsync(Expected);
+        var mockLogger = new Mock<ILogger<CommentController>>();
+        
 
-    // Then
+        var controller = new CommentController(mockLogger.Object, mockRepository.Object);
+
+        // Act
+        var result = await controller.CreateComment(createCommentDto);
+
+        // Assert
+        Assert.Equal(Expected.commentDTO, result.comment);
+        Assert.Equal(Expected.Created, result.status);       
+    }
 }
