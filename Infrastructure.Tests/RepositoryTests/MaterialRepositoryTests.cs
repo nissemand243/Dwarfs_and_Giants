dd8d6343e242b4c1fd3edf6ed947d418fc796990
coha <coha@itu.dk>
2021-12-18 13:24:39 +0100

    Testfix

--- Infrastructure.Tests/RepositoryTests/MaterialRepositoryTests.cs
+++ Infrastructure.Tests/RepositoryTests/MaterialRepositoryTests.cs
@@ -19,9 +19,9 @@ public class MaterialRepositoryTests : IDisposable
         var _context = new DatabaseContext(builder.Options);
         _context.Database.EnsureCreated();
         _context.Materials.AddRange(
-            new Material(){Id = 1, AuthorId = 1, Name = "Docker is (not) fun", Description = "blabladockerblabla",FileType = FileType.Pdf, FilePath = "MaterialsData/test.pdf" },
-            new Material(){Id = 2, AuthorId = 1, Name = "C# global using functionality", Description = "Global using is an easy way to import packages globally",FileType = FileType.Pdf, FilePath = "MaterialsData/test.pdf" },
-            new Material(){Id = 3, AuthorId = 2, Name = "SwEng theory is tough", Description = "Theory for Software engineering is difficult, but very useful when developing larger systems.", FileType = FileType.Pdf, FilePath = "MaterialsData/test.pdf" });
+            new Material(){Id = 1, AuthorId = 1, Name = "Docker is (not) fun", Description = "blabladockerblabla",FileType = FileType.pdf, FilePath = "MaterialsData/test.pdf" },
+            new Material(){Id = 2, AuthorId = 1, Name = "C# global using functionality", Description = "Global using is an easy way to import packages globally",FileType = FileType.pdf, FilePath = "MaterialsData/test.pdf" },
+            new Material(){Id = 3, AuthorId = 2, Name = "SwEng theory is tough", Description = "Theory for Software engineering is difficult, but very useful when developing larger systems.", FileType = FileType.pdf, FilePath = "MaterialsData/test.pdf" });
         _context.SaveChanges();
 
         context = _context;
@@ -31,10 +31,10 @@ public class MaterialRepositoryTests : IDisposable
      [Fact]
     public async void CreateAsync_returns_matching_material()
     {
-        var material = await repo.CreateMaterialAsync(new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.Pdf.ToString(),
+        var material = await repo.CreateMaterialAsync(new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.pdf.ToString(),
             "MaterialsData/test.pdf"));
 
-        var expected = new MaterialDTO(4,2, "Testing", "Testing for dummies.", FileType.Pdf.ToString(),
+        var expected = new MaterialDTO(4,2, "Testing", "Testing for dummies.", FileType.pdf.ToString(),
             "MaterialsData/test.pdf");
         Assert.Equal(expected ,material.Item2);
     } 
@@ -42,14 +42,14 @@ public class MaterialRepositoryTests : IDisposable
     [Fact]
     public async void CreateAsync_given_new_entity_returns_created()
     {
-        var material = await repo.CreateMaterialAsync(new MaterialDTO(42, 2, "Testing", "Testing for dummies.", FileType.Pdf.ToString(),
+        var material = await repo.CreateMaterialAsync(new MaterialDTO(42, 2, "Testing", "Testing for dummies.", FileType.pdf.ToString(),
             "MaterialsData/test.pdf"));
         Assert.Equal(Status.Created,material.Item1);
     }
 
     [Fact] public async void UpdateMaterialAsync_given_id_and_DTO_returns_updated()
     {
-        var actual = await repo.UpdateMaterialAsync(2, new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.Pdf.ToString(),
+        var actual = await repo.UpdateMaterialAsync(2, new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.pdf.ToString(),
             "MaterialsData/test.pdf"));
         
         Assert.Equal(Updated, actual);
@@ -57,7 +57,7 @@ public class MaterialRepositoryTests : IDisposable
     
     [Fact] public async void UpdateMaterialAsync_given_non_existing_id_returns_NotFound()
     {
-        var actual = await repo.UpdateMaterialAsync(42, new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.Pdf.ToString(),
+        var actual = await repo.UpdateMaterialAsync(42, new CreateMaterialDTO(2, "Testing", "Testing for dummies.", FileType.pdf.ToString(),
             "MaterialsData/test.pdf"));
         
         Assert.Equal(NotFound, actual);
@@ -76,18 +76,18 @@ public class MaterialRepositoryTests : IDisposable
     {
         var material2 = await repo.ReadAsync(2);
         
-        var expMaterial = new MaterialDTO(2, 1, "C# global using functionality", "Global using is an easy way to import packages globally", "Pdf","MaterialsData/test.pdf" );
+        var expMaterial = new MaterialDTO(2, 1, "C# global using functionality", "Global using is an easy way to import packages globally", "pdf","MaterialsData/test.pdf" );
         Assert.Equal(expMaterial, material2);
     }
 
     [Fact]
     public async void ReadAllAsync_returns_all_material()
     {
-        var expMaterialList = new List<MaterialDTO>{new (1, 1, "Docker is (not) fun", "blabladockerblabla", "Pdf", "MaterialsData/test.pdf"),
+        var expMaterialList = new List<MaterialDTO>{new (1, 1, "Docker is (not) fun", "blabladockerblabla", "pdf", "MaterialsData/test.pdf"),
         new (2, 1, "C# global using functionality",
-            "Global using is an easy way to import packages globally", "Pdf", "MaterialsData/test.pdf"),
+            "Global using is an easy way to import packages globally", "pdf", "MaterialsData/test.pdf"),
         new (3, 2, "SwEng theory is tough",
-            "Theory for Software engineering is difficult, but very useful when developing larger systems.", "Pdf",
+            "Theory for Software engineering is difficult, but very useful when developing larger systems.", "pdf",
             "MaterialsData/test.pdf")
     };
         var actual = await repo.ReadAllAsync();
