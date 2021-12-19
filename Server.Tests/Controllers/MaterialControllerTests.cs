17ab3515616042ba1bb0e5ee7ac4f815f96f8fa8
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-19 21:30:21 +0100

    Add readAll materials test

--- Server.Tests/Controllers/MaterialControllerTests.cs
+++ Server.Tests/Controllers/MaterialControllerTests.cs
@@ -6,8 +6,8 @@ public class MaterialControllerTests
     public async Task CreateMaterial_creates_material_returns_material_and_status()
     {
         // Arrange
-        var createMaterialDTO = new CreateMaterialDTO(1,"Lorem 3.", "Lorem material", FileType.Link, "Google.com");
-        var materialDTO = new MaterialDTO(1,1,"Lorem the 3.", "Lorem material",FileType.Link, "Google.com");
+        var createMaterialDTO = new CreateMaterialDTO(1,"Lorem 3.", "Lorem material", Link, "Google.com");
+        var materialDTO = new MaterialDTO(1,1,"Lorem the 3.", "Lorem material",Link, "Google.com");
         var expected = (Status.Created, materialDTO);
         
         var repository = new Mock<IMaterialRepository>();
@@ -29,7 +29,7 @@ public class MaterialControllerTests
     public async Task ReadMaterial_given_existing_id_returns_MaterialDTO()
     {
         // Arrange
-        var expected = new MaterialDTO(1,2, "Lorem 2.", "Lorem material", FileType.Pdf, "Google.com");
+        var expected = new MaterialDTO(1,2, "Lorem 2.", "Lorem material", Pdf, "Google.com");
         var logger = new Mock<ILogger<MaterialController>>();
         var repository = new Mock<IMaterialRepository>();
         repository.Setup(repo => repo.ReadAsync(1)).ReturnsAsync(expected);
@@ -63,10 +63,23 @@ public class MaterialControllerTests
     public async Task ReadAll_returns_materialDTo()
     {
         // Arrange
-        
-    
+        var materialDTOOne = new MaterialDTO(0, 1, "Lorem", "Ipsum master", Mp4, "url.com"); 
+        var materialDTOTwo = new MaterialDTO(0, 2, "Ipsum", "Lorem master", Mp4, "url.com"); 
+
+        var expected = new List<MaterialDTO>(){materialDTOOne, materialDTOTwo}.AsReadOnly();
+        var logger = new Mock<ILogger<MaterialController>>();
+        var repository = new Mock<IMaterialRepository>();
+        repository.Setup(repo => repo.ReadAllAsync()).ReturnsAsync(expected);
+        var controller = new MaterialController(logger.Object, repository.Object);
+
+
         // Act
-    
+        var actual = await controller.ReadAllAsync();
+
         // Assert
+        Assert.Collection(actual,
+            aDTO => Assert.Equal(expected[0].Name, aDTO.Name),
+            aDTO => Assert.Equal(expected[1].Name, aDTO.Name)
+        );
     }
 }
