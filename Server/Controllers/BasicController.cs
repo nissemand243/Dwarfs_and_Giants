0f0a7bbab2b287d89e56d470f50d0952328d3bce
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-20 19:21:54 +0100

    refactor dataannotation paths

--- Server/Controllers/BasicController.cs
+++ Server/Controllers/BasicController.cs
@@ -22,7 +22,7 @@ public class BasicController : ControllerBase, IBasicController
         _searchEngine = searchEngine;
     }
     [Authorize(Roles = $"{Roles.Teacher},{Roles.Student},{Roles.Administrator},{Roles.User}")]
-    [HttpGet("{id}")]
+    [HttpGet("Material/{MaterialID}")]
     public async Task<(Status, DetailsMaterialDTO?)> Get(int id)
     {
         var detailedDto = await _searchEngine.GetDetailedMaterialByIdAsync(id);
@@ -34,7 +34,7 @@ public class BasicController : ControllerBase, IBasicController
     }
 
     [Authorize(Roles = $"{Roles.Teacher},{Roles.Student},{Roles.Administrator},{Roles.User}")]
-    [HttpPatch("{MaterialId}")]
+    [HttpPatch("Material/{MaterialID}")]
     [ProducesResponseType(StatusCodes.Status201Created)]
     public Task<(Status status, CommentDTO comment)> PatchComment(CreateCommentDTO comment)
     {
@@ -42,14 +42,14 @@ public class BasicController : ControllerBase, IBasicController
     }
 
     [Authorize(Roles = $"{Roles.Teacher},{Roles.Student},{Roles.Administrator},{Roles.User}")]
-    [HttpPatch("{MaterialID}")] // find a different way to have two patch way methods
+    [HttpPatch("{MaterialID}")]
     public Task<(Status status, RatingDTO rating)> PatchRating(CreateRatingDTO rating)
     {
         return _ratingController.CreateRating(rating);
     }
 
     [Authorize(Roles = $"{Roles.Teacher},{Roles.Student},{Roles.Administrator},{Roles.User}")]
-    [HttpGet]
+    [HttpGet("{SearchString}")]
     public Task<IReadOnlyCollection<MaterialDTO>> Search(string searchInput)
     {
         return _searchEngine.SearchAsync(searchInput);
