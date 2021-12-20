eaddf3a235116e87a491bc6de66da82365b86559
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-20 23:16:41 +0100

    Add userclaims
    to create user dto from claims

---
+++ Client/UserClaims.cs
@@ -0,0 +1,20 @@
+
+
+public class UserClaims
+{
+    public static CreateUserDTO? CreateUserOrDefault(ClaimsPrincipal userClaims)
+    {
+        if(userClaims.Identity?.IsAuthenticated == true)
+        {
+            var claims = userClaims.Claims;
+            var email = claims.Where(c => c.Type == "preferred_username").SingleOrDefault();
+            var name = claims.Where(c => c.Type == "name").SingleOrDefault();
+            if(email != null && name != null)
+            {
+                var userDTO = new CreateUserDTO(name.ToString(), email.ToString());
+                return userDTO;
+            }
+        }            
+        return null;
+    }
+}
