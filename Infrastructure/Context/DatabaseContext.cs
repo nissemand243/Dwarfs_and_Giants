24c554bad5759c6c2d01b51e19dbd75e2cbe7d25
coha <coha@itu.dk>
2021-12-21 14:09:38 +0100

    change the database contex

--- Infrastructure/Context/DatabaseContext.cs
+++ Infrastructure/Context/DatabaseContext.cs
@@ -13,31 +13,13 @@ public class DatabaseContext : DbContext, IDatabaseContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>()
-                    .HasKey(u => u.Email);
-
-        modelBuilder.Entity<User>()
-                    .HasIndex(u => u.Id)
-                    .IsUnique();
-                   
+        modelBuilder.Entity<User>().ToTable("User");
                     
         modelBuilder.Entity<Material>().ToTable("Material");
 
         modelBuilder.Entity<Tag>().ToTable("Tag");
+        
         modelBuilder.Entity<Rating>().ToTable("Rating");
-        /*modelBuilder.Entity<Tag>()
-                    .HasIndex(t => t.MaterialId)
-                    .HasIndex(t => t.TagName)
-                    .IsUnique();
-
-        modelBuilder.Entity<Rating>()
-                    .HasIndex(r => r.MaterialId)
-                    .HasIndex(r => r.UserId)
-                    .IsUnique();*/
-
-        /*modelBuilder.Entity<Comment>()
-                    .Property(c => c.Text)
-                    .HasMaxLength(500);*/
 
         modelBuilder.Entity<Comment>().ToTable("Comment");
 
