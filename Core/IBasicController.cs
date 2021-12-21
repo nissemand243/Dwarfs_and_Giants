5ea980c5043e125b51cf456d7fa2dfcb6b71d81f
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-12-21 03:32:42 +0100

    Remove method signatures
    as ActionResult is not possible as the return type

--- Core/IBasicController.cs
+++ Core/IBasicController.cs
@@ -2,8 +2,5 @@
 
 public interface IBasicController
 {
-    Task<(Status status, CommentDTO comment)> PatchComment(CreateCommentDTO comment);
-    Task<(Status status, RatingDTO rating)> PatchRating(CreateRatingDTO rating);
-    Task<(Status, DetailsMaterialDTO)> Get(int id);
     Task<IReadOnlyCollection<MaterialDTO>> Search(string searchInput);
 }
